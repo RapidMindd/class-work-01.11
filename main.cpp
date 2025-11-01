33cdef0435d06d372c128a91797355ee017cec1a
RapidMindd <YaroslavT007@yandex.ru>
2025-11-01 19:51:45 +0300

    input/output added

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 void destroy(int ** mtx, size_t created) {
-    for (size_t i = 0; i < created; i ++) {
+    for (size_t i = 0; i < created; ++i) {
         delete[] mtx[i];
     }
     delete[] mtx;
@@ -12,9 +12,8 @@ int ** create(size_t rows, size_t cols) {
     size_t created = 0;
     int ** mtx = new int * [rows];
     try {
-        for (; created < rows; created ++) {
+        for (; created < rows; ++created) {
             mtx[created] = new int[cols];
-            created += 1;
         }
     } catch (const std::bad_alloc &e) {
         destroy(mtx, created);
@@ -23,14 +22,55 @@ int ** create(size_t rows, size_t cols) {
     return mtx;
 }
 
+int ** construct(int ** mtx, int init, size_t rows, size_t cols) {
+    for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < cols; ++j) {
+            mtx[i][j] = init;
+        }
+    }
+    return mtx;
+}
+
+void input (int ** mtx, size_t rows, size_t cols) {
+    for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < cols; ++j) {
+            std::cin >> mtx[i][j];
+        }
+    }
+}
+
+void output (int ** mtx, size_t rows, size_t cols) {
+    for (size_t i = 0; i < rows; ++i) {
+        std::cout << mtx[i][0];
+        for (size_t j = 1; j < cols; ++j) {
+            std::cout << " " << mtx[i][j];
+        }
+        std::cout << "\n";
+    }
+}
+
 int main ()
 {
+    size_t r = 0, c = 0;
+    std::cin >> r >> c;
+    if (!std::cin) {
+        std::cerr << "incorrect input\n";
+        return 1;
+    }
     int ** matrix = nullptr;
     try {
-        matrix = create(5, 5);
+        matrix = create(r, c);
     } catch (const std::bad_alloc & e) {
         std::cerr << e.what() << "\n";
         return 1; 
     }
-    destroy(matrix, 5);
+    construct(matrix, 0, r, c);
+    input (matrix, r, c);
+    if (!std::cin) {
+        std::cerr << "incorrect input\n";
+        destroy(matrix, r);
+        return 1;
+    }
+    output(matrix, r, c);
+    destroy(matrix, r);
 }
